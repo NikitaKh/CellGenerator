6a5dfcbe27e3e5a33c9f31671971b9bd495b9727
NK <nk@MacBook-Pro-NK.local>
2020-01-28 01:12:29 +0300

    Cell generator

---
+++ CellGen/src/ru/yandex/task/gen.java
@@ -0,0 +1,55 @@
+package ru.yandex.task;
+
+public class gen {
+        public static void map_gen(int x, int y) {
+            int[][] matrix = new int[x][y];
+            for (int i = 0; i < x; i++) {
+                for (int j = 0; j < y; j++) {
+                    matrix[i][j] = (int) (Math.random() * 2);
+                }
+            }
+                counter(matrix);
+        }
+        public static int counter(int[][] matrix) {
+            reader.print(matrix);
+                try {
+                    Thread.sleep(1000);
+                } catch(InterruptedException ex)
+                {}
+                for (int i = 0; i < matrix.length; i++) {
+                    for (int j = 0; j < matrix[i].length; j++) {
+                        if (i == 0 && j == 0) {
+                            corners.first_cell_top(matrix);
+                        }
+                        if (i == 0 && j == matrix[i].length - 1) {
+                            corners.last_cell_top(matrix);
+                        }
+                        if (i == matrix.length - 1 && j == 0) {
+                            corners.first_cell_bot(matrix);
+                        }
+                        if (i == matrix.length - 1 &&
+                                j == matrix[matrix.length - 1].length - 1) {
+                            corners.last_cell_bot(matrix);
+                        }
+                        if (i == 0 && (j >= 1 && j < matrix[i].length - 1)) {
+                            lines.zero_line(matrix, j);
+                        }
+                        if ((i >= 1 && i < matrix.length - 1) && j == 0) {
+                            lines.left_line(matrix, i);
+                        }
+                        if ((i >= 1 && i < matrix.length - 1) && j == matrix[i].length - 1) {
+                            lines.right_line(matrix, i, j);
+                        }
+                        if (i == matrix.length - 1 && (j >= 1 && j < matrix[i].length - 1)) {
+                            lines.last_line(matrix, i, j);
+                        }
+                        if ((i >= 1 && i <= matrix.length - 2) && (j >= 1 && j <= matrix[i].length - 2)) {
+                            lines.middle_lines(matrix, i, j);
+                        }
+
+                    }
+                }
+                int[][] subMatrix = matrix;
+                return(counter(subMatrix));
+        }
+}
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # CellGenerator
+
+    The board is given with size M x N cell.  Each cell can be  in one of two stages: 1 - alive, 0 - dead.
+Each cell coop with the next 8 neighbours. 
+    Rules of cooperation:
+
+        A living cell that has fewer than two living neighbors dies.
+
+        A living cell that has two or three living neighbors survives.
+
+        A living cell that has more than three living neighbors dies.
+
+        A dead cell that has three living neighbors is reborn.
+The program can generate map and represent it onec in a second, of it is possible to receive map from file.
